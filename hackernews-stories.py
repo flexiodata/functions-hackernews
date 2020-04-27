1c64922b54faa0f958df477b2f9014a1df40336d
Aaron Williams <aaron@flex.io>
2020-04-27 17:40:20 -0500

    added support for pagination and header/limit parameters

--- hackernews-stories.py
+++ hackernews-stories.py
@@ -1,8 +1,8 @@
 # ---
-# name: hackernews-search-stories
+# name: hackernews-stories
 # deployed: true
-# title: Hacker News Search Stories
-# description: Returns the 100-most-relevant Hacker News stories matching the search term
+# title: Hacker Stories
+# description: Returns the the Hacker News stories matching the search term
 # params:
 #   - name: properties
 #     type: array
@@ -47,79 +47,112 @@ from decimal import *
 from cerberus import Validator
 from collections import OrderedDict
 
+# main function entry point
 def flexio_handler(flex):
 
     # get the input
     input = flex.input.read()
-    try:
-        input = json.loads(input)
-        if not isinstance(input, list): raise ValueError
-    except ValueError:
-        raise ValueError
+    input = json.loads(input)
+    if not isinstance(input, list):
+        input = []
 
     # define the expected parameters and map the values to the parameter names
     # based on the positions of the keys/values
     params = OrderedDict()
     params['properties'] = {'required': False, 'validator': validator_list, 'coerce': to_list, 'default': '*'}
-    params['search'] = {'required': True, 'type': 'string'}
+    params['filter'] = {'required': False, 'type': 'string', 'default': ''} # placeholder to match form of index-styled functions
+    params['config'] = {'required': False, 'type': 'string', 'default': ''} # index-styled config string
     input = dict(zip(params.keys(), input))
 
     # validate the mapped input against the validator
     v = Validator(params, allow_unknown = True)
-    input = v.validated(input)
-    if input is None:
+    params = v.validated(input)
+    if params is None:
         raise ValueError
 
-    # map this function's property names to the API's property names
-    property_map = OrderedDict()
-    property_map['title'] = 'title'
-    property_map['url'] = 'url'
-    property_map['author'] = 'author'
-    property_map['points'] = 'points'
-    property_map['num_comments'] = 'num_comments'
-    property_map['created_at'] = 'created_at'
+    # get the properties to return and the property map;
+    # if we have a wildcard, get all the properties
+    properties = [p.lower().strip() for p in input['properties']]
+    if len(properties) == 1 and (properties[0] == '' or properties[0] == '*'):
+        properties = list(get_item_info({}).keys())
 
-    try:
+    # get any configuration settings
+    config = urllib.parse.parse_qs(input['config'])
+    config = {k: v[0] for k, v in config.items()}
+    limit = int(config.get('limit', 100))
+    if limit >= 1000:
+        limit = 1000
+    headers = config.get('headers', 'true').lower()
+    if headers == 'true':
+        headers = True
+    else:
+        headers = False
 
-        # make the request
-        # see here for more info:
-        # - https://hn.algolia.com/api
-        # - https://www.algolia.com/doc/api-reference/api-parameters/
-        url_query_params = {
-            "query": input["search"],
-            "tags": "story",
-            "hitsPerPage": 100,
-            "restrictSearchableAttributes": "title",    # limit search to title
-            "disableTypoToleranceOnAttributes": "title" # don't allow typos when searching for words
-        }
-        url_query_str = urllib.parse.urlencode(url_query_params)
+    # write the output
+    flex.output.content_type = 'application/json'
+    flex.output.write('[')
 
-        url = 'https://hn.algolia.com/api/v1/search?' + url_query_str
-        response = requests_retry_session().get(url)
-        content = response.json()
-
-        # get the properties to return and the property map
-        properties = [p.lower().strip() for p in input['properties']]
-
-        # if we have a wildcard, get all the properties
-        if len(properties) == 1 and properties[0] == '*':
-            properties = list(property_map.keys())
-
-        # build up the result
-        result = []
-
-        result.append(properties)
-        stories = content.get('hits',[])
-        for item in stories:
-            row = [item.get(property_map.get(p,''),'') or '' for p in properties]
-            result.append(row)
-
-        result = json.dumps(result, default=to_string)
-        flex.output.content_type = "application/json"
+    first_row = True
+    if headers is True:
+        result = json.dumps(properties)
+        first_row = False
         flex.output.write(result)
 
-    except:
-        raise RuntimeError
+    for item in get_data(params, limit):
+        result = json.dumps([item.get(p) for p in properties])
+        if first_row is False:
+            result = ',' + result
+        first_row = False
+        flex.output.write(result)
+
+    flex.output.write(']')
+
+def get_data(params, limit):
+
+    # see here for more info:
+    # - https://hn.algolia.com/api
+    # - https://www.algolia.com/doc/api-reference/api-parameters/
+
+    url_query_params = {
+        "query": params["filter"],
+            "tags": "story",
+            "restrictSearchableAttributes": "title",    # limit search to title
+            "disableTypoToleranceOnAttributes": "title" # don't allow typos when searching for words
+    }
+    url = 'https://hn.algolia.com/api/v1/search'
+
+    page_size = 100
+    page_idx = 0
+    row_idx = 0
+    while True:
+
+        url_query_params['hitsPerPage'] = page_size
+        url_query_params['page'] = page_idx
+        url_query_str = urllib.parse.urlencode(url_query_params)
+        page_url = url + '?' + url_query_str
+
+        response = requests_retry_session().get(page_url)
+        content = response.json()
+        data = content.get('hits',[])
+
+        if len(data) == 0: # sanity check in case there's an issue with cursor
+            break
+
+        for item in data:
+            if row_idx >= limit:
+                break
+            yield get_item_info(item)
+            row_idx = row_idx + 1
+
+        page_idx = content.get('page')
+        page_count = content.get('nbPages')
+
+        if page_idx is None:
+            break
+        if page_idx >= page_count-1:
+            break
+        if row_idx >= limit:
+            break
 
 def requests_retry_session(
     retries=3,
@@ -166,3 +199,16 @@ def to_list(value):
         return list(itertools.chain.from_iterable(value))
     return None
 
+def get_item_info(item):
+
+    # map this function's property names to the API's property names
+    info = OrderedDict()
+
+    info['title'] = item.get('title')
+    info['url'] = item.get('url')
+    info['author'] = item.get('author')
+    info['points'] = item.get('points')
+    info['num_comments'] = item.get('num_comments')
+    info['created_at'] = item.get('created_at')
+
+    return info
